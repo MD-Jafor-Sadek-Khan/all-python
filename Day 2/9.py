0b14191c4ada8babf50bfb53cf816a09a65f1e90
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2023-04-01 20:07:00 +0600

    i work

--- Day 2/9.py
+++ Day 2/9.py
@@ -17,3 +17,23 @@ print("poped one is "+kala)
 friends.insert(2,"chocho dodo")
 print(friends)
 
+friends.sort()
+print(friends)
+
+num.sort()
+print(num)
+
+num.reverse()
+print(num)
+
+num.remove(2)
+print(num)
+
+friends=["kala","dhola","kauwa","pota"]
+num=[2,4,5,6,2,13,5,4,5]
+
+print(friends.index("kala"))
+
+print(friends.index("dhola"))
+
+print(num.count(2))
